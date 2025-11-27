407300d74773341593f2efc07d189c9d13d1c1d8
João Fernandes <imjoaofernandes@gmail.com>
2025-11-27 23:40:27 +0000

    feat: add ShlfWatch Watch App with initial structure, including ContentView and app entry point, along with asset configurations

---
+++ ShlfWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShlfWatch Watch App
+//
+//  Created by João Fernandes on 27/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShlfWatch Watch App/ShlfWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShlfWatchApp.swift
+//  ShlfWatch Watch App
+//
+//  Created by João Fernandes on 27/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShlfWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
